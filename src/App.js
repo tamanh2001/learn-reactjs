7344901a131fa26281e5cd46d17270a9dab67272
Tam Anh <Tamanh180501@gmail.com>
2021-08-12 21:36:57 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoFeature from "./features/Todo";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return ( <
+        div class = 'App' > { < TodoFeature / > }
+
+        <
+        /div>
+    );
+
 }
 
 export default App;
